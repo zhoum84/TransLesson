b3ad811337beccda06ae4174599a6def431cdb42
serhiimytsyk <mytsyk.s.v@gmail.com>
2023-10-28 18:53:04 -0500

    translation ready

--- backend/src/main/java/vandyhacks/translesson/backend/controller/TranscriptController.java
+++ backend/src/main/java/vandyhacks/translesson/backend/controller/TranscriptController.java
@@ -1,7 +1,6 @@
 package vandyhacks.translesson.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import vandyhacks.translesson.backend.model.Transcript;
 import vandyhacks.translesson.backend.service.TranscriptService;
--- backend/src/test/java/vandyhacks/translesson/backend/BackendApplicationTests.java
+++ backend/src/test/java/vandyhacks/translesson/backend/BackendApplicationTests.java
@@ -2,12 +2,17 @@ package vandyhacks.translesson.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import vandyhacks.translesson.backend.service.TranscriptService;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        var service = new TranscriptService(null);
+        System.out.println(service.translate(Arrays.asList("hello", "I love you"), "es"));
     }
 
 }
