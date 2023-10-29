836485b70e3eb4879c476ef41d9a7ae75c73417e
serhiimytsyk <mytsyk.s.v@gmail.com>
2023-10-29 00:08:49 -0500

    michael cors 5

--- backend/src/main/java/vandyhacks/translesson/backend/controller/TranscriptController.java
+++ backend/src/main/java/vandyhacks/translesson/backend/controller/TranscriptController.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class TranscriptController {
     private final TranscriptService service;
 
@@ -22,38 +22,45 @@ public class TranscriptController {
         this.service = service;
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping(value = "/transcripts", params = {"name", "language"})
     public List<String> getByNameAndLanguage(@RequestParam String name, @RequestParam String language) {
+        System.out.println("Get 1" + name + " " + language);
         return service.getTextsByNameAndLanguage(name, language);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping(value = "/transcripts", params = {"date", "language"})
     public List<String> getByDateAndLanguage(@RequestParam LocalDate date, @RequestParam String language) {
+        System.out.println("Get 2" + date + " " + language);
         return service.getTextsByDateAndLanguage(date, language);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/transcripts")
-    public String addTranscript(@RequestBody Transcript t) {
+    public ResponseEntity<String> addTranscript(@RequestBody Transcript t) {
         System.out.println(t.getText());
-        return service.addTranscript(t);
+        return getCorsResponse(service.addTranscript(t));
     }
 
     private <T> ResponseEntity<T> getCorsResponse(T t) {
         HttpHeaders headers = new HttpHeaders();
-
+        System.out.println("Post" + t);
         // Add CORS headers
         headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        headers.add("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS");
         headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         headers.add("Access-Control-Allow-Credentials", "true");
 
         return new ResponseEntity<>(t, headers, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/transcripts", method = RequestMethod.OPTIONS)
     public void optionsRequest(HttpServletResponse response) {
+        System.out.println("Options");
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         response.setHeader("Access-Control-Allow-Credentials", "true");
     }
